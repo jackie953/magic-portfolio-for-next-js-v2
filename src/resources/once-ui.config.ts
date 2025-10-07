8dd2a315193fe49c5b55b7117790258b2e186c55
Jacqueline Mhitaryan <jackie@mhitaryan.com>
2025-10-08 01:03:53 +0200

    fixes

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -3,7 +3,6 @@ import type {
   DisplayConfig,
   EffectsConfig,
   FontsConfig,
-  MailchimpConfig,
   ProtectedRoutesConfig,
   RoutesConfig,
   SameAsConfig,
@@ -143,50 +142,6 @@ const effects: EffectsConfig = {
   },
 };
 
-const mailchimp: MailchimpConfig = {
-  action: "https://url/subscribe/post?parameters",
-  effects: {
-    mask: {
-      cursor: true,
-      x: 50,
-      y: 0,
-      radius: 100,
-    },
-    gradient: {
-      display: true,
-      opacity: 90,
-      x: 50,
-      y: 0,
-      width: 50,
-      height: 50,
-      tilt: 0,
-      colorStart: "accent-background-strong",
-      colorEnd: "static-transparent",
-    },
-    dots: {
-      display: true,
-      opacity: 20,
-      size: "2",
-      color: "brand-on-background-weak",
-    },
-    grid: {
-      display: false,
-      opacity: 100,
-      color: "neutral-alpha-medium",
-      width: "0.25rem",
-      height: "0.25rem",
-    },
-    lines: {
-      display: false,
-      opacity: 100,
-      color: "neutral-alpha-medium",
-      size: "16",
-      thickness: 1,
-      angle: 90,
-    },
-  },
-};
-
 // default schema data
 const schema: SchemaConfig = {
   logo: "",
@@ -221,7 +176,6 @@ const socialSharing: SocialSharingConfig = {
 
 export {
   display,
-  mailchimp,
   routes,
   protectedRoutes,
   baseURL,
