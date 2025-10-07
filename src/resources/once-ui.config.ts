2681ac938f63d0d0cb1a4b8b543d842c5071df56
Jacqueline Mhitaryan <jackie@mhitaryan.com>
2025-10-07 03:10:08 +0200

    fixed work, blog names

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -19,8 +19,8 @@ const baseURL: string = "https://demo.magic-portfolio.com";
 const routes: RoutesConfig = {
   "/": true,
   "/about": true,
-  "/projects": true,
-  "/insights": true,
+  "/work": true,
+  "/blog": true,
   "/gallery": false,
 };
 
