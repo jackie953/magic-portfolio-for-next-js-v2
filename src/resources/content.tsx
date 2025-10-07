39ae87c34b51ef9d208e6f171589c6ea4dc135fc
Jacqueline Mhitaryan <jackie@mhitaryan.com>
2025-10-07 03:39:57 +0200

    syntax

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -5,7 +5,7 @@ const person: Person = {
   firstName: "",
   lastName: "",
   name: "Mhitaryan Works",
-  role: ""
+  role: "",
   avatar: "/images/logo 500x500",
   email: "jackie@mhitaryan.com",
   location: "Europe/Stockholm", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
